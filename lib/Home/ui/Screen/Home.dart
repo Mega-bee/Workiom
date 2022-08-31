f4773f3a72727f4ce5c51a8aef4ba4eef59b02f4
jadsaidd <jadsaidd@gmail.com>
2022-08-31 10:38:48 +0300

    TEST TEST

--- lib/Home/ui/Screen/Home.dart
+++ lib/Home/ui/Screen/Home.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:workiom/Home/ui/Widgets/Apps.dart';
+import 'package:workiom/utils/style/Colors.dart';
 import '../../../utils/Images/Images.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -17,51 +20,98 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text("T-Technology"),
-        titleTextStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 32),
+        titleTextStyle: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 32,
+            color: Color(0xFF0E0F12)),
         elevation: 0,
         actions: [
           IconButton(onPressed: () {}, icon: Image.asset(ImageAsset.WORKSPACE))
         ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.05,
-              ),
-              child: TextField(
-                controller: _textEditingController,
-                autofocus: false,
-                onChanged: (searchText) {},
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(
-                      vertical: MediaQuery.of(context).size.height * 0.01),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(50)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Theme.of(context).primaryColor)),
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Jump to',
-                  // suffixIcon: const ImageIcon(
-                  //   AssetImage("assets/images/down,-filter,-list,-sort.png"),
-                  //   color: Color.fromRGBO(18, 108, 242, 1),
-                  // ),
-                  hintStyle: const TextStyle(
-                      color: Color(0xFF555555),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'Rubik'),
-                ),
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 60),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+            child: TextField(
+              controller: _textEditingController,
+              autofocus: false,
+              onChanged: (searchText) {},
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor)),
+                filled: true,
+                fillColor: Color(0xFFF4F4F4),
+                hintText: 'Jump to...',
+                // suffixIcon: const ImageIcon(
+                //   AssetImage("assets/images/down,-filter,-list,-sort.png"),
+                //   color: Color.fromRGBO(18, 108, 242, 1),
+                // ),
+                hintStyle: const TextStyle(
+                    color: Color(0xFF555555),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Rubik'),
               ),
             ),
-            
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+    child:Padding(
+        padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+        child: Column(
+          children: [
+            Card(
+              elevation: 0,
+                color: PrimaryColor,
+                child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "Favorite",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Rubik'),
+                    ),
+                    Spacer(),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "View all",
+                          style: TextStyle(
+                              color: Color(0xFF747474),
+                              fontFamily: 'Rubik',
+                              fontSize: 14,
+                          ),
+                        ))
+                  ],
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height*0.3,
+                  child: ListView.builder(
+                    shrinkWrap: false,
+                    physics: BouncingScrollPhysics(
+                        parent: AlwaysScrollableScrollPhysics()),
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      return Container();
+                    },
+                  ),
+                )
+              ],
+            )),
+            Apps()
+            // Container(child: Apps(),)
           ],
         ),
       ),
-    );
+    ));
   }
 }
